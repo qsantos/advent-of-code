1c02b38f61bc389b16fd43633a38be6a75cedc7c
Quentin Santos <qsantos@qsantos.fr>
2021-12-17 17:45:13 +0100

    Bidirectional breadth-first-search for 2016-12-11

--- 2016/day11/main.py
+++ 2016/day11/main.py
@@ -3,6 +3,7 @@ from typing import List, Set, Tuple
 
 Things = List[List[int]]
 State = Tuple[int, Tuple[Tuple[int, ...], ...]]
+Front = List[State]
 
 
 def read_initial_state(filename: str) -> Things:
@@ -65,41 +66,54 @@ def freeze(lift: int, things: Things) -> State:
     return (lift, tuple(sorted(tuple(thing) for thing in things)))
 
 
+def expand_front(n_floors: int, q: Front, seen: Set[State]) -> Front:
+    next_q = []
+    for lift, things in q:
+        candidates = []
+        for i, (microchip, generator) in enumerate(things):
+            if microchip == lift:
+                candidates.append((i, 0))
+            if generator == lift:
+                candidates.append((i, 1))
+        for dh in (-1, 1):
+            if not 0 <= lift + dh < n_floors:
+                continue
+            for n_things in (1, 2):
+                for luggage in combinations(candidates, r=n_things):
+                    new_lift = lift + dh
+                    new_things = [list(thing) for thing in things]
+                    for kind, is_generator in luggage:
+                        new_things[kind][is_generator] += dh
+                    if not is_valid(new_things):
+                        continue
+                    new_state = freeze(new_lift, new_things)
+                    if new_state in seen:
+                        continue
+                    seen.add(new_state)
+                    next_q.append(new_state)
+    return next_q
+
+
 def solve(things: Things) -> int:
     n_floors = 4
-    q = [(0, things)]
-    seen: Set[State] = {freeze(0, things)}
-    steps = 0
-    while q:
-        next_q = []
-        for lift, things in q:
-            if all_on_floor(things, n_floors - 1):
-                return steps
-            candidates = []
-            for i, (microchip, generator) in enumerate(things):
-                if microchip == lift:
-                    candidates.append((i, 0))
-                if generator == lift:
-                    candidates.append((i, 1))
-            for dh in (-1, 1):
-                if not 0 <= lift + dh < n_floors:
-                    continue
-                for n_things in (1, 2):
-                    for luggage in combinations(candidates, r=n_things):
-                        new_lift = lift + dh
-                        new_things = [list(thing) for thing in things]
-                        for kind, is_generator in luggage:
-                            new_things[kind][is_generator] += dh
-                        if not is_valid(new_things):
-                            continue
-                        new_state = freeze(new_lift, new_things)
-                        if new_state in seen:
-                            continue
-                        seen.add(new_state)
-                        next_q.append((new_lift, new_things))
-        q = next_q
-        steps += 1
-    assert False
+    forward_start = freeze(0, things)
+    backward_start = freeze(3, [[3, 3] for _ in things])
+    forward_front = [forward_start]
+    backward_front = [backward_start]
+    forward_seen: Set[State] = {forward_start}
+    backward_seen: Set[State] = {backward_start}
+    forward_steps = 0
+    backward_steps = 0
+    while forward_front or backward_front:
+        forward_front = expand_front(n_floors, forward_front, forward_seen)
+        forward_steps += 1
+        if forward_seen & backward_seen:
+            return forward_steps + backward_steps
+        backward_front = expand_front(n_floors, backward_front, backward_seen)
+        backward_steps += 1
+        if forward_seen & backward_seen:
+            return forward_steps + backward_steps
+    return -1
 
 
 def main() -> None:
