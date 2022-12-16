7a4f4a4b70f12d41ce808dfde033872f9e746b8a
Quentin Santos <qsantos@qsantos.fr>
2022-12-16 21:51:05 +0100

    2022-12-15 Part 2

--- 2022/day15/src/main.rs
+++ 2022/day15/src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::HashSet;
+use std::cmp::Ord;
+use std::collections::BinaryHeap;
 
 #[derive(Eq, Hash, PartialEq)]
 struct Coord {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Coord {
@@ -14,15 +15,18 @@ impl Coord {
         Coord { x, y }
     }
 
-    fn distance(&self, other: &Self) -> u32 {
+    fn distance(&self, other: &Self) -> u64 {
         self.x.abs_diff(other.x) + self.y.abs_diff(other.y)
+    }
+
+    fn tuning_frequency(&self) -> i64 {
+        self.x * 4_000_000 + self.y
     }
 }
 
 struct Sensor {
     position: Coord,
-    closest_beacon: Coord,
-    cell_radius: u32,
+    cell_radius: u64,
 }
 
 impl Sensor {
@@ -33,44 +37,108 @@ impl Sensor {
         let radius = position.distance(&beacon);
         Sensor {
             position,
-            closest_beacon: beacon,
             cell_radius: radius,
         }
     }
 }
 
-fn count_not_beacons_at(filename: &str, y: i32) -> usize {
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum BoundType {
+    Close,
+    Open,
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct IntervalBound {
+    position: i64,
+    kind: BoundType,
+}
+
+impl IntervalBound {
+    fn open(position: i64) -> Self {
+        IntervalBound {
+            position,
+            kind: BoundType::Open,
+        }
+    }
+    fn close(position: i64) -> Self {
+        IntervalBound {
+            position,
+            kind: BoundType::Close,
+        }
+    }
+}
+
+fn count_not_beacons_at(filename: &str, y: i64) -> i64 {
     let contents = std::fs::read_to_string(filename).unwrap();
     let sensors: Vec<Sensor> = contents.lines().map(Sensor::read).collect();
-    let beacons: HashSet<&Coord> = sensors
-        .iter()
-        .map(|sensor| &sensor.closest_beacon)
-        .collect();
+    let mut bounds = BinaryHeap::new();
+    for sensor in sensors {
+        let dy = sensor.position.y.abs_diff(y);
+        if sensor.cell_radius < dy {
+            continue;
+        }
+        let rem = (sensor.cell_radius - dy) as i64;
+        bounds.push(IntervalBound::open(sensor.position.x + rem));
+        bounds.push(IntervalBound::close(sensor.position.x - rem));
+    }
 
-    let min_x = sensors
-        .iter()
-        .map(|sensor| {
-            sensor.position.x - (sensor.cell_radius as i32 - sensor.position.y.abs_diff(y) as i32)
-        })
-        .min()
-        .unwrap();
-    let max_x = sensors
-        .iter()
-        .map(|sensor| {
-            sensor.position.x + (sensor.cell_radius as i32 - sensor.position.y.abs_diff(y) as i32)
-        })
-        .max()
-        .unwrap();
+    let mut count = 0;
+    let mut depth = 0u64;
+    let mut first_open = None;
+    while let Some(bound) = bounds.pop() {
+        match bound.kind {
+            BoundType::Open => {
+                if depth == 0 {
+                    first_open = Some(bound.position);
+                }
+                depth += 1;
+            }
+            BoundType::Close => {
+                depth -= 1;
+                if depth == 0 {
+                    count += first_open.unwrap() - bound.position;
+                    first_open = None;
+                }
+            }
+        };
+    }
+    count
+}
 
-    (min_x..=max_x)
-        .filter(|&x| {
-            let c = Coord { x, y };
-            !beacons.contains(&c)
-                && sensors
-                    .iter()
-                    .any(|sensor| c.distance(&sensor.position) <= sensor.cell_radius)
-        })
-        .count()
+fn find_distress_beacon(filename: &str, size: i64) -> i64 {
+    let contents = std::fs::read_to_string(filename).unwrap();
+    let sensors: Vec<Sensor> = contents.lines().map(Sensor::read).collect();
+
+    for y in 0..size {
+        let mut bounds = BinaryHeap::new();
+        for sensor in &sensors {
+            let dy = sensor.position.y.abs_diff(y);
+            if sensor.cell_radius < dy {
+                continue;
+            }
+            let rem = (sensor.cell_radius - dy) as i64;
+            bounds.push((sensor.position.x + rem, sensor.position.x - rem));
+        }
+
+        let mut cover_start = size;
+        while let Some((end, start)) = bounds.pop() {
+            if end < cover_start - 1 {
+                let c = Coord {
+                    x: cover_start - 1,
+                    y,
+                };
+                return c.tuning_frequency();
+            }
+            if start < cover_start {
+                cover_start = start;
+            }
+            if cover_start <= 0 {
+                break;
+            }
+        }
+    }
+    unreachable!();
 }
 
 fn puzzle1() {
@@ -79,8 +147,8 @@ fn puzzle1() {
 }
 
 fn puzzle2() {
-    // println!("{}", count_not_beacons_at("example", 10));
-    // println!("{}", count_not_beacons_at("input", 2_000_000));
+    assert_eq!(find_distress_beacon("example", 20), 56000011);
+    assert_eq!(find_distress_beacon("input", 4_000_000), 10961118625406);
 }
 
 fn main() {
