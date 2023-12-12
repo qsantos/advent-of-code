606cf5ff0a3406ed98918787b1549e7eaf49705c
Quentin Santos <qsantos@qsantos.fr>
2023-12-12 08:15:28 +0100

    2023-12-12 part 2

--- 2023/day12/src/main.rs
+++ 2023/day12/src/main.rs
@@ -2,38 +2,48 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn arrangements(springs: &[u8], groups: &Vec<i32>) -> usize {
-    fn f(springs: &[u8], i: usize, groups: &Vec<i32>, j: usize, current_group_size: i32) -> usize {
-        if i == springs.len() {
-            if j == groups.len() || (j == groups.len() - 1 && current_group_size == *groups.last().unwrap()) {
-                return 1;
-            } else {
-                return 0;
-            }
-        } else if j == groups.len() {
-            if springs[i..].iter().any(|s| *s == b'#') {
-                return 0;
-            } else {
-                return 1;
-            }
+    // c[i][j] is the number of arrangements when looking at the first i springs and j groups
+    let mut c = Vec::new();
+
+    {
+        let mut first_row = Vec::new();
+        first_row.push(1);
+        for _ in 1..=groups.len() {
+            first_row.push(0);
         }
-        let spring = springs[i];
-        let mut count = 0;
-        if spring == b'.' || spring == b'?' {
-            if current_group_size != 0 {
-                let group = groups[j];
-                if current_group_size == group {
-                    count += f(springs, i + 1, groups, j + 1, 0);
-                }
-            } else {
-                count += f(springs, i + 1, groups, j, 0);
-            }
-        }
-        if spring == b'#' || spring == b'?' {
-            count += f(springs, i + 1, groups, j, current_group_size + 1);
-        }
-        count
+        c.push(first_row);
     }
-    f(springs, 0, groups, 0, 0)
+
+    for i in 1..=springs.len() {
+        let spring = springs[i - 1];
+        let mut row = Vec::new();
+        for j in 0..=groups.len() {
+            let mut count = 0;
+            // consume no group
+            if spring == b'.' || spring == b'?' {
+                count += c[i - 1][j];
+            }
+            // consume a single group
+            if j > 0 {
+                let group_size = groups[j - 1] as usize;
+                // group elements
+                if i >= group_size && springs[(i - 1) - (group_size - 1)..=(i - 1)].iter().all(|s| *s == b'#' || *s == b'?') {
+                    if i == group_size {
+                        count += c[0][j - 1];
+                    } else {
+                        // group separator
+                        let s = springs[(i - 1) - (group_size - 1) - 1];
+                        if s == b'.' || s == b'?' {
+                            count += c[i - group_size - 1][j - 1];
+                        }
+                    }
+                }
+            }
+            row.push(count);
+        }
+        c.push(row);
+    }
+    c[springs.len()][groups.len()]
 }
 
 fn part1(filename: &str) -> usize {
@@ -53,7 +63,30 @@ fn part1(filename: &str) -> usize {
     sum
 }
 
+fn part2(filename: &str) -> usize {
+    let f = File::open(filename).unwrap();
+    let mut reader = BufReader::new(f);
+    let mut buf = String::new();
+    let mut sum = 0;
+    while reader.read_line(&mut buf).unwrap() != 0 {
+        let line = buf.trim();
+        let (springs, groups) = line.split_once(' ').unwrap();
+        let springs = springs.as_bytes();
+        let springs = [springs, springs, springs, springs, springs].join(&b'?');
+        let groups: Vec<i32> = groups.split(',').map(|g| g.parse().unwrap()).collect();
+        let groups = groups.repeat(5);
+        let c = arrangements(&springs, &groups);
+        dbg!(line, c);
+        sum += c;
+        buf.clear();
+    }
+    sum
+}
+
 fn main() {
     assert_eq!(part1("example"), 21);
     assert_eq!(part1("input"), 6852);
+
+    assert_eq!(part2("example"), 525152);
+    assert_eq!(part2("input"), 8475948826693);
 }
