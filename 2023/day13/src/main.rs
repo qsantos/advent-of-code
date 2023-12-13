3f435142ca27f381d18ebddc6fde8a8b54e9f4ed
Quentin Santos <qsantos@qsantos.fr>
2023-12-13 18:29:41 +0100

    2023-12-13 part 2

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -6,27 +6,32 @@ fn transpose(block: &[&[u8]]) -> Vec<Vec<u8>> {
         .collect()
 }
 
-fn middle_row<R: AsRef<[u8]>>(block: &[R]) -> Option<usize> {
+fn hamming(a: &[u8], b: &[u8]) -> usize {
+    a.iter().zip(b.iter()).map(|(a, b)| if a == b { 0 } else { 1 }).sum()
+}
+
+fn middle_row<R: AsRef<[u8]>>(block: &[R], d: usize) -> Option<usize> {
     let rows = block.len();
     for middle in 1..rows {
         let width = middle.min(rows - middle);
-        if (0..width).all(|i| block[middle - 1 - i].as_ref() == block[middle + i].as_ref()) {
+        let h: usize = (0..width).map(|i| hamming(block[middle - 1 - i].as_ref(), block[middle + i].as_ref())).sum();
+        if h == d {
             return Some(middle);
         }
     }
     None
 }
 
-fn part1(filename: &str) -> usize {
+fn part12(filename: &str, d: usize) -> usize {
     let data = std::fs::read_to_string(filename).unwrap();
     let mut sum = 0;
     for block in data.trim().split("\n\n") {
         let block: Vec<&[u8]> = block.as_bytes().split(|b| *b == b'\n').collect();
-        if let Some(middle) = middle_row(&block) {
+        if let Some(middle) = middle_row(&block, d) {
             sum += middle * 100;
         } else {
             let block = transpose(&block);
-            let middle = middle_row(&block).unwrap();
+            let middle = middle_row(&block, d).unwrap();
             sum += middle;
         }
     }
@@ -34,6 +39,9 @@ fn part1(filename: &str) -> usize {
 }
 
 fn main() {
-    assert_eq!(part1("example"), 405);
-    assert_eq!(part1("input"), 37975);
+    assert_eq!(part12("example", 0), 405);
+    assert_eq!(part12("input", 0), 37975);
+
+    assert_eq!(part12("example", 1), 400);
+    assert_eq!(part12("input", 1), 32497);
 }
