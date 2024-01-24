31d3d0c639d2c0ea1d6471815583cb61cdba035e
Quentin Santos <qsantos@qsantos.fr>
2024-01-24 08:21:18 +0100

    2023-12-21 Only track frontier

--- 2023/day21/src/main.rs
+++ 2023/day21/src/main.rs
@@ -3,42 +3,54 @@ use std::collections::HashSet;
 fn part1(filename: &str, steps: usize) -> usize {
     let data = std::fs::read_to_string(filename).unwrap();
     let grid: Vec<&[u8]> = data.lines().map(|line| line.as_bytes()).collect();
-    let rows = grid.len();
-    let cols = grid[0].len();
+    let rows = grid.len() as isize;
+    let cols = grid[0].len() as isize;
 
     let start = grid
         .iter()
         .enumerate()
-        .flat_map(|(i, row)| row.iter().position(|c| *c == b'S').map(|j| (i, j)))
+        .flat_map(|(i, row)| {
+            row.iter()
+                .position(|c| *c == b'S')
+                .map(|j| (i as isize, j as isize))
+        })
         .next()
         .unwrap();
 
-    let mut q = vec![start];
-    for _ in 0..steps {
-        let mut next = HashSet::new();
-        for (i, j) in q {
-            let mut candidates = Vec::new();
-            if i > 0 {
-                candidates.push((i - 1, j));
-            }
-            if j > 0 {
-                candidates.push((i, j - 1));
-            }
-            if i < rows - 1 {
-                candidates.push((i + 1, j));
-            }
-            if j < cols - 1 {
-                candidates.push((i, j + 1));
-            }
-            for (ni, nj) in candidates {
-                if grid[ni][nj] != b'#' {
-                    next.insert((ni, nj));
+    let mut previous = HashSet::new();
+    let mut current = HashSet::new();
+    let mut next = HashSet::new();
+    current.insert(start);
+    let mut odds = 0;
+    let mut evens = 1; // {start} at step 0
+    for step in 1..=steps {
+        next.clear();
+        for (i, j) in current.iter().copied() {
+            for (ni, nj) in [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)] {
+                if !(0 <= ni && ni < rows && 0 <= nj && nj < cols) {
+                    continue;
                 }
+                if grid[ni as usize][nj as usize] == b'#' {
+                    continue;
+                }
+                if previous.contains(&(ni, nj)) {
+                    continue;
+                }
+                next.insert((ni, nj));
             }
         }
-        q = next.into_iter().collect();
+        (previous, current, next) = (current, next, previous);
+        if step % 2 == 0 {
+            evens += current.len();
+        } else {
+            odds += current.len();
+        }
     }
-    q.len()
+    if steps % 2 == 0 {
+        evens
+    } else {
+        odds
+    }
 }
 
 fn main() {
