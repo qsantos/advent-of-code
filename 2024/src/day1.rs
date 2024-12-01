96dfa9c111372feb8d2788b1e46ce19b3a186224
Quentin Santos <qsantos@qsantos.fr>
2024-12-01 10:26:05 +0100

    2024-12-01 part 2

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,6 +1,7 @@
+use std::collections::BTreeMap;
 use std::fmt::Display;
 
-pub fn part1(input: &str) -> impl Display {
+fn parse_lists(input: &str) -> (Vec<u64>, Vec<u64>) {
     let mut left = Vec::new();
     let mut right = Vec::new();
     for line in input.lines() {
@@ -10,11 +11,27 @@ pub fn part1(input: &str) -> impl Display {
         left.push(l);
         right.push(r);
     }
+    (left, right)
+}
+
+pub fn part1(input: &str) -> impl Display {
+    let (mut left, mut right) = parse_lists(input);
     left.sort();
     right.sort();
     left.into_iter()
         .zip(right)
         .map(|(l, r)| l.abs_diff(r))
+        .sum::<u64>()
+}
+
+pub fn part2(input: &str) -> impl Display {
+    let (left, right) = parse_lists(input);
+    let mut counts = BTreeMap::new();
+    for v in right.into_iter() {
+        *counts.entry(v).or_insert(0) += 1;
+    }
+    left.into_iter()
+        .map(|v| v * counts.get(&v).unwrap_or(&0))
         .sum::<u64>()
 }
 
@@ -35,5 +52,11 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1(EXAMPLE).to_string(), "11");
         assert_eq!(part1(INPUT).to_string(), "1660292");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE).to_string(), "31");
+        assert_eq!(part2(INPUT).to_string(), "22776016");
     }
 }
