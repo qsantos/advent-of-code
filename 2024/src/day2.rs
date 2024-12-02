eed7dde5b51a6691eb93720275c349b5e46974b7
Quentin Santos <qsantos@qsantos.fr>
2024-12-02 07:56:10 +0100

    2024-12-02 don't restore levels when not needed

--- 2024/src/day2.rs
+++ 2024/src/day2.rs
@@ -35,31 +35,28 @@ fn is_safe_increasing(levels: &[u32]) -> bool {
     find_unsafe_pair_increasing(levels).is_none()
 }
 
-fn is_safe_increasing_tolerance(levels: &mut Vec<u32>) -> bool {
-    let Some(i) = find_unsafe_pair_increasing(levels) else {
+fn is_safe_increasing_tolerance(mut levels: Vec<u32>) -> bool {
+    let Some(i) = find_unsafe_pair_increasing(&levels) else {
         return true;
     };
-    let mut original = levels.remove(i - 1);
-    let safe = is_safe_increasing(levels);
+    let original = levels.remove(i - 1);
+    let safe = is_safe_increasing(&levels);
     if safe {
-        levels.insert(i - 1, original);
         return true;
     }
     // combine the two operations below in one to avoid moving all elements
-    (levels[i - 1], original) = (original, levels[i - 1]);
+    levels[i - 1] = original;
     //levels.insert(i - 1, original);
-    //let original = levels.remove(i);
-    let safe = is_safe_increasing(levels);
-    levels.insert(i, original);
-    safe
+    //levels.remove(i);
+    is_safe_increasing(&levels)
 }
 
 fn is_safe(levels: &[u32]) -> bool {
     is_safe_decreasing(levels) || is_safe_increasing(levels)
 }
 
-fn is_safe_tolerance(levels: &mut Vec<u32>) -> bool {
-    is_safe_decreasing_tolerance(levels) || is_safe_increasing_tolerance(levels)
+fn is_safe_tolerance(mut levels: Vec<u32>) -> bool {
+    is_safe_decreasing_tolerance(&mut levels) || is_safe_increasing_tolerance(levels)
 }
 
 pub fn part1(input: &str) -> impl Display {
@@ -76,8 +73,8 @@ pub fn part1(input: &str) -> impl Display {
 pub fn part2(input: &str) -> impl Display {
     let mut safe_reports = 0;
     for line in input.lines() {
-        let mut levels: Vec<u32> = line.split(' ').map(|x| x.parse().unwrap()).collect();
-        if is_safe_tolerance(&mut levels) {
+        let levels: Vec<u32> = line.split(' ').map(|x| x.parse().unwrap()).collect();
+        if is_safe_tolerance(levels) {
             safe_reports += 1;
         }
     }
