3e5845f86092286c8cd1fa83a8270a0c2ea69226
Quentin Santos <qsantos@qsantos.fr>
2024-12-04 06:32:51 +0100

    2024-12-04 part 1

---
+++ 2024/src/day4.rs
@@ -0,0 +1,52 @@
+use std::fmt::Display;
+
+pub fn part1(input: &str) -> impl Display {
+    let needle = b"XMAS";
+    let grid: Vec<&[u8]> = input.trim().as_bytes().split(|&b| b == b'\n').collect();
+    let mut count = 0;
+    let rows = grid.len() as i64;
+    let cols = grid[0].len() as i64;
+    let dirs = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+        [1, 1],
+        [1, -1],
+        [-1, 1],
+        [-1, -1],
+    ];
+    for i in 0..rows {
+        for j in 0..cols {
+            for dir in dirs {
+                count += 1;
+                for (a, &n) in needle.iter().enumerate() {
+                    let ni = i + a as i64 * dir[0];
+                    let nj = j + a as i64 * dir[1];
+                    if !(0..rows).contains(&ni)
+                        || !(0..cols).contains(&nj)
+                        || grid[ni as usize][nj as usize] != n
+                    {
+                        count -= 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../examples/day4.txt");
+    const INPUT: &str = include_str!("../inputs/day4.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(EXAMPLE).to_string(), "18");
+        assert_eq!(part1(INPUT).to_string(), "2532");
+    }
+}
