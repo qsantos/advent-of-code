26605761bfc970e15901f0e6ad7a914ae203164e
Quentin Santos <qsantos@qsantos.fr>
2024-12-06 06:55:58 +0100

    2024-12-06 part 2

--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -12,30 +12,54 @@ fn find_start(grid: &[&[u8]]) -> (i64, i64) {
     unreachable!()
 }
 
-pub fn part1(input: &str) -> impl Display {
-    let grid: Vec<&[u8]> = input.trim().as_bytes().split(|&b| b == b'\n').collect();
+fn parse(input: &str) -> Vec<&[u8]> {
+    input.trim().as_bytes().split(|&b| b == b'\n').collect()
+}
+
+fn path(grid: &[&[u8]], obstable: Option<(usize, usize)>) -> Option<HashSet<(usize, usize)>> {
     let rows = grid.len() as i64;
     let cols = grid[0].len() as i64;
-    let (mut i, mut j) = find_start(&grid);
+    let (mut i, mut j) = find_start(grid);
     let (mut di, mut dj) = (-1, 0); // up
     let mut visited = HashSet::new();
+    let mut states = HashSet::new();
     while (0..rows).contains(&i) && (0..cols).contains(&j) {
-        println!("{i} {j} {di} {dj}");
-        visited.insert((i, j));
+        if !states.insert((i, j, di, dj)) {
+            return None;
+        }
+        visited.insert((i as usize, j as usize));
         loop {
             let (ni, nj) = (i + di, j + dj);
             if !(0..rows).contains(&ni) || !(0..cols).contains(&nj) {
                 (i, j) = (ni, nj);
                 break;
             }
-            if grid[ni as usize][nj as usize] != b'#' {
+            let (si, sj) = (ni as usize, nj as usize);
+            if Some((si, sj)) != obstable && grid[si][sj] != b'#' {
                 (i, j) = (ni, nj);
                 break;
             }
             (di, dj) = (dj, -di)
         }
     }
-    visited.len()
+    Some(visited)
+}
+
+pub fn part1(input: &str) -> impl Display {
+    let grid = parse(input);
+    path(&grid, None).unwrap().len()
+}
+
+pub fn part2(input: &str) -> impl Display {
+    let grid = parse(input);
+    let mut count = 0;
+    let visited = path(&grid, None).unwrap();
+    for (i, j) in visited {
+        if path(&grid, Some((i, j))).is_none() {
+            count += 1;
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -49,5 +73,11 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1(EXAMPLE).to_string(), "41");
         assert_eq!(part1(INPUT).to_string(), "4665");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE).to_string(), "6");
+        assert_eq!(part2(INPUT).to_string(), "1688");
     }
 }
