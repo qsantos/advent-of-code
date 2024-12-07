ea919c5f7447321848bf655e98ba10c704879407
Quentin Santos <qsantos@qsantos.fr>
2024-12-07 06:13:46 +0100

    2024-12-07 part 2

--- 2024/src/day7.rs
+++ 2024/src/day7.rs
@@ -22,6 +22,36 @@ pub fn part1(input: &str) -> impl Display {
     total
 }
 
+pub fn part2(input: &str) -> impl Display {
+    let mut total = 0;
+    for line in input.lines() {
+        let (left, right) = line.split_once(": ").unwrap();
+        let left: u64 = left.parse().unwrap();
+        let right: Vec<u64> = right.split(' ').map(|x| x.parse().unwrap()).collect();
+        fn aux(left: u64, right: &[u64], i: usize, acc: u64) -> bool {
+            if i == right.len() {
+                return acc == left;
+            }
+            if acc > left {
+                return false;
+            }
+            if aux(left, right, i + 1, acc + right[i]) {
+                return true;
+            }
+            if aux(left, right, i + 1, acc * right[i]) {
+                return true;
+            }
+            let digits = right[i].ilog10() + 1;
+            let combined = acc * 10u64.pow(digits) + right[i];
+            aux(left, right, i + 1, combined)
+        }
+        if aux(left, &right, 1, right[0]) {
+            total += left;
+        }
+    }
+    total
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,5 +63,11 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1(EXAMPLE).to_string(), "3749");
         assert_eq!(part1(INPUT).to_string(), "882304362421");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE).to_string(), "11387");
+        assert_eq!(part2(INPUT).to_string(), "145149066755184");
     }
 }
