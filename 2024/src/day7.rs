b4ab7739186f55da587afa87a3aedbd474b46be6
Quentin Santos <qsantos@qsantos.fr>
2024-12-07 06:08:43 +0100

    2024-12-07 part 1

---
+++ 2024/src/day7.rs
@@ -0,0 +1,41 @@
+use std::fmt::Display;
+
+pub fn part1(input: &str) -> impl Display {
+    let mut total = 0;
+    for line in input.lines() {
+        let (left, right) = line.split_once(": ").unwrap();
+        let left: u64 = left.parse().unwrap();
+        let right: Vec<u64> = right.split(' ').map(|x| x.parse().unwrap()).collect();
+        fn aux(left: u64, right: &[u64], i: usize, acc: u64) -> usize {
+            if i == right.len() {
+                if acc == left {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+            if acc > left {
+                return 0;
+            }
+            aux(left, right, i + 1, acc + right[i]) + aux(left, right, i + 1, acc * right[i])
+        }
+        if aux(left, &right, 1, right[0]) != 0 {
+            total += left;
+        }
+    }
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("../examples/day7.txt");
+    const INPUT: &str = include_str!("../inputs/day7.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(EXAMPLE).to_string(), "3749");
+        assert_eq!(part1(INPUT).to_string(), "882304362421");
+    }
+}
