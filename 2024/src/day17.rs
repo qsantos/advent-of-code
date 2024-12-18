100ce5c88c3ed7709d042c6b06085c72efafb3d0
Quentin Santos <qsantos@qsantos.fr>
2024-12-18 17:28:08 +0100

    2024-12-17 part 2

--- 2024/src/day17.rs
+++ 2024/src/day17.rs
@@ -1,4 +1,3 @@
-use std::collections::HashSet;
 use std::fmt::Display;
 
 #[derive(Debug)]
@@ -139,86 +138,81 @@ pub fn part1(input: &str) -> impl Display {
     format_output(&state.output)
 }
 
-pub fn part2(input: &str) -> impl Display {
-    let mut state = State::from_input(input);
-    // number of numbers in the output that should match the program
-    let mut matching_output_prefix_length = 4;
-    // numbers of least-significant bits that are restricted to the set of candidates
-    let mut set_bits_count = 1;
-    // candidates for set least-significant bits
-    let mut set_bits_candidates = HashSet::new();
-    set_bits_candidates.insert(0u64);
-    set_bits_candidates.insert(1u64);
-    loop {
-        let mut candidates = HashSet::new();
-        let mut a = 0;
-        while candidates.len() < 100 {
-            for suffix in set_bits_candidates.iter() {
-                let candidate = (a << set_bits_count) | suffix;
-                state.evaluate(a);
-                if state.output.len() < matching_output_prefix_length {
-                    continue;
-                }
-                let output_prefix = &state.output[..matching_output_prefix_length];
-                let program_prefix = &state.program[..matching_output_prefix_length];
-                if output_prefix != program_prefix {
-                    continue;
-                }
-                candidates.insert(candidate);
-            }
-            a += 1;
-        }
-        set_bits_candidates = candidates
-            .iter()
-            .map(|c| c & ((1 << set_bits_count) - 1))
-            .collect();
-        loop {
-            set_bits_count += 1;
-            let new_set_bits_candidates: HashSet<_> = candidates
-                .iter()
-                .map(|c| c & ((1 << set_bits_count) - 1))
-                .collect();
-            for candidate in &new_set_bits_candidates {
-                print!("{:20b}", candidate);
-            }
-            println!();
-            if new_set_bits_candidates.len() > 5 {
-                set_bits_count -= 1;
-                break;
-            }
-            matching_output_prefix_length += 1;
-            set_bits_candidates = new_set_bits_candidates;
-        }
-        break;
-    }
-    panic!("");
+pub fn part2_example(_input: &str) -> impl Display {
+    let numbers = [0, 3, 5, 4, 3, 0];
 
-    let ends: Vec<u64> = vec![0, 1];
-    let ends: Vec<u64> = vec![0b10110101, 0b10111101];
-    let ends: Vec<u64> = vec![
-        0b10100100111100110110101,
-        0b11011010110111010111101,
-        0b11011010110111110111101,
-    ];
-    let n = ends.iter().max().unwrap().ilog2() + 1;
+    // The example contains the following pairs of numbers, which correspond to the sequence of
+    // instructions below:
+    // 0,3  A = A >> 3
+    // 5,4  output A % 8
+    // 3,0  if A != 0 goto 0
 
-    let prefix = 14;
-
-    let mut candidates = HashSet::new();
+    // That means that each tribit of A directly maps to a number of the output. We can easily
+    // reconstruct A from the output:
     let mut a = 0;
-    loop {
-        for end in ends {
-            let candidate = a << n | end;
-            state.evaluate(candidate);
-            if state.output == state.program {
-                return a;
-            }
-            if state.output.len() >= prefix && state.output[..prefix] == state.program[..prefix] {
-                candidates.insert(candidate);
+    for number in numbers.into_iter().rev() {
+        a = a << 3 | number;
+    }
+    // the program _starts_ by shifting A, so we need to shift one more time here
+    a << 3
+}
+
+pub fn part2(_input: &str) -> impl Display {
+    let numbers = [2, 4, 1, 1, 7, 5, 1, 5, 4, 3, 0, 3, 5, 5, 3, 0];
+
+    // My input contains the following pairs of numbers, which correspond to the sequence of
+    // instructions below:
+    // 2,4  B = A % 8
+    // 1,1  B = B ^ 1
+    // 7,5  C = A >> B
+    // 1,5  B = B ^ 5
+    // 4,3  B = B ^ C
+    // 0,3  A = A >> 3
+    // 5,5  output B % 8
+    // 3,0  if A != 0 goto 0
+
+    // This correspond to this pseudo-code, with `round_output` being defined below.
+    // ```
+    // do {
+    //     output round_output(A);
+    //     A = A >> 3
+    // } while (A != 0);
+    // ```
+    fn round_output(a: u64) -> u64 {
+        let b = a % 8;
+        let b = b ^ 1;
+        let c = a >> b;
+        let b = b ^ 5;
+        let b = b ^ c;
+        b % 8
+    }
+
+    // From this, we can see that there is one tribit in A for each number in the output.
+    //
+    // To reconstruct A, we start from the most significant tribit, which corresponds to the last
+    // number of the program. Only certain values of that tribit will output the required number (0
+    // here). We keep all possible values, and move to the previous number of the program.
+    //
+    // We then iterate for each number in reverse order, trying all possible values of the tribit
+    // for each of the candidates of the previous round. We need to keep all the candidates, since
+    // some of the candidates might not output a certain number for any tribit we add to it.
+    let mut candidates = vec![0];
+    let mut new_candidates = Vec::new();
+    for number in numbers.into_iter().rev() {
+        new_candidates.clear();
+        for candidate in candidates.iter().copied() {
+            for tribit in 0..8 {
+                let new_candidate = candidate << 3 | tribit;
+                if round_output(new_candidate) == number {
+                    new_candidates.push(new_candidate);
+                }
             }
         }
-        a += 1;
+        (candidates, new_candidates) = (new_candidates, candidates);
     }
+    // candidates are kept in sorted order, so the smallest possible solution is the first
+    // remaining candidate
+    candidates[0]
 }
 
 #[cfg(test)]
@@ -237,24 +231,7 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        //assert_eq!(part2(EXAMPLE2).to_string(), "117440");
+        assert_eq!(part2_example(EXAMPLE2).to_string(), "117440");
         assert_eq!(part2(INPUT).to_string(), "164541017976509");
     }
 }
-
-/*
-2,4 B = A % 8
-1,1 B = B ^ 1
-7,5 C = A >> B
-1,5 B = B ^ 5
-4,3 B = B ^ C
-0,3 A = A >> 3
-5,5 output B % 8
-3,0 if A != 0 goto 0
-
-do {
-    B = (A % 8) ^ 1
-    output B ^ 5 ^ (A >> B) % 8
-    A = A >> 3
-} while (A != 0);
-*/
