d42901e797c43a8fc7ea21d61b7eefcb3833fd45
Quentin Santos <qsantos@qsantos.fr>
2024-12-19 06:46:11 +0100

    2024-12-19 part 2

--- 2024/src/day19.rs
+++ 2024/src/day19.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 
 pub fn part1(input: &str) -> impl Display {
@@ -27,6 +28,37 @@ pub fn part1(input: &str) -> impl Display {
     count
 }
 
+pub fn part2(input: &str) -> impl Display {
+    let (towels, designs) = input.split_once("\n\n").unwrap();
+    let mut towels: Vec<&str> = towels.split(", ").collect();
+    let designs: Vec<&str> = designs.lines().collect();
+    towels.sort_by_key(|towel| -(towel.len() as isize));
+
+    let mut memo = HashMap::new();
+    fn aux<'a>(towels: &[&str], design: &'a str, memo: &mut HashMap<&'a str, usize>) -> usize {
+        if design.is_empty() {
+            return 1;
+        }
+        if let Some(&count) = memo.get(design) {
+            return count;
+        }
+        let mut count = 0;
+        for towel in towels {
+            if let Some(suffix) = design.strip_prefix(towel) {
+                count += aux(towels, suffix, memo);
+            }
+        }
+        memo.insert(design, count);
+        count
+    }
+
+    let mut count = 0;
+    for design in designs {
+        count += aux(&towels, design, &mut memo);
+    }
+    count
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +70,11 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1(EXAMPLE).to_string(), "6");
         assert_eq!(part1(INPUT).to_string(), "216");
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE).to_string(), "16");
+        assert_eq!(part2(INPUT).to_string(), "603191454138773");
     }
 }
