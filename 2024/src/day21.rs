46d5a0b34add89b0725845bb69abd3092ade5db8
Quentin Santos <qsantos@qsantos.fr>
2024-12-25 11:52:59 +0100

    2024-12-21 part 2 attempt 3

--- 2024/src/day21.rs
+++ 2024/src/day21.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::fmt::Display;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -37,189 +37,242 @@ const NUM_START: Point = Point::new(3, 2);
 const DIR_KEYPAD: [&[u8]; 2] = [b" ^A", b"<v>"];
 const DIR_START: Point = Point::new(0, 2);
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct State<const N: usize> {
-    typed: i32,
-    numerical_pos: Point,
-    directional_pos: [Point; N],
+// NOTES:
+// - When typing a number, all the robots have to be pressing "A". This ensures that typing the
+//   numbers is is independent from the others.
+// - Similarly, the state at an intermediate layer only depends on what has been typed so far.
+// - When going from one button to another, the shortest path always uses one of up/down and
+//   one of left/right. Additional buttons are never needed, even to optimize the presses of
+//   higher-level robots.
+// - The optimal series of up/down and left/right presses is either, all up/down, then all
+//   left/right, then back to press A, or all left/right, then all up/down, then back to press
+//   A.
+// - The two approaches are always equivalent in terms of number of presses, since moves are
+//   time-symmetric. For instance:
+//   - A, move to ">", press 3 times, move to "^", press 2 times, move to "A", press "A".
+//   - A, move to "^", press 2 times, move to ">", press 3 times, move to "A", press "A".
+// The first two points allow us to take a greedy approach at each level. The last two points
+// allow us to efficiently remember the needed information at each level to find the number of
+// required steps.
+
+// For instance, to press '5', we can do '<^^A'. For this, we need to do the following on the
+// level above:
+// - 'v' once
+// - '<' twice
+// - 'A' once
+// - '>' once
+// - '^' once
+// - 'A' twice
+// - '>' once
+// - 'A' once
+// In other words, '<^^A' is mapped to 'v<A', '>^AA', '>A'.
+
+// di > 0 corresponds to the number of v
+// di < 0 corresponds to the number of ^
+// dj > 0 corresponds to the number of >
+// dj < 0 corresponds to the number of <
+// a > 0 corresponds to the number of A
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Action {
+    di: i8,
+    dj: i8,
+    a: i8,
 }
 
-impl<const N: usize> State<N> {
-    const fn new() -> Self {
-        Self {
-            typed: 0,
-            numerical_pos: NUM_START,
-            directional_pos: [DIR_START; N],
-        }
-    }
-
-    fn act(&self, line: &[u8], mut action: u8) -> Option<Self> {
-        let mut new_state = self.clone();
-        let mut depth = 0;
-        while depth < N && action == b'A' {
-            let p = self.directional_pos[depth];
-            action = DIR_KEYPAD[p.i as usize][p.j as usize];
-            depth += 1;
-        }
-        if action == b'A' {
-            // type on numeric keypad
-            assert_eq!(depth, N);
-            let pos = self.numerical_pos;
-            let c = NUM_KEYPAD[pos.i as usize][pos.j as usize];
-            if c != line[self.typed as usize] {
-                return None;
-            }
-            new_state.typed += 1;
-            Some(new_state)
-        } else if depth == N {
-            // move robot on numeric keypad
-            let (di, dj) = match action {
-                b'<' => (0, -1),
-                b'>' => (0, 1),
-                b'^' => (-1, 0),
-                b'v' => (1, 0),
-                _ => panic!("Invalid action '{}'", action as char),
-            };
-            let pos = &mut new_state.numerical_pos;
-            let i = pos.i + di;
-            if !(0..4).contains(&i) {
-                return None;
-            }
-            let j = pos.j + dj;
-            if !(0..3).contains(&j) {
-                return None;
-            }
-            if NUM_KEYPAD[i as usize][j as usize] == b' ' {
-                return None;
-            }
-            new_state.numerical_pos = Point::new(i, j);
-            Some(new_state)
-        } else {
-            // move robot on directional keypad
-            let (di, dj) = match action {
-                b'<' => (0, -1),
-                b'>' => (0, 1),
-                b'^' => (-1, 0),
-                b'v' => (1, 0),
-                _ => panic!("Invalid action '{}'", action as char),
-            };
-            let pos = new_state.directional_pos[depth];
-            let i = pos.i + di;
-            if !(0..2).contains(&i) {
-                return None;
-            }
-            let j = pos.j + dj;
-            if !(0..3).contains(&j) {
-                return None;
-            }
-            if DIR_KEYPAD[i as usize][j as usize] == b' ' {
-                return None;
-            }
-            new_state.directional_pos[depth] = Point::new(i, j);
-            Some(new_state)
-        }
-    }
-}
-
-#[test]
-fn test_state_act() {
-    fn evaluate_state<const N: usize>(line: &[u8], actions: &[u8]) {
-        let mut state: State<N> = State::new();
-        for &action in actions {
-            state = state.act(line, action).unwrap();
-        }
-        assert_eq!(state.typed, line.len() as i32);
-    }
-    evaluate_state::<0>(b"029A", b"<A^A>^^AvvvA");
-    evaluate_state::<0>(b"029A", b"<A^A^>^AvvvA");
-    evaluate_state::<0>(b"029A", b"<A^A^^>AvvvA");
-    evaluate_state::<1>(b"029A", b"v<<A>>^A<A>AvA<^AA>A<vAAA>^A");
-    evaluate_state::<2>(
-        b"029A",
-        b"<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
-    );
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct CostState<const N: usize> {
-    cost: i32, // accumulated cost so far
-    state: State<N>,
-}
-
-impl<const N: usize> CostState<N> {
-    fn new() -> Self {
-        Self {
-            cost: 0,
-            state: State::new(),
-        }
-    }
-
-    /// Lower-bound estimate of the total cost
-    fn f(&self) -> i32 {
-        self.g() + self.h()
-    }
-
-    /// Accumulated cost so far
-    fn g(&self) -> i32 {
-        self.cost
-    }
-
-    /// Lower-bound estimate of the remaining cost to reach the goal
-    fn h(&self) -> i32 {
-        -self.state.typed
-            + self.state.numerical_pos.dist(NUM_START)
-            + self
-                .state
-                .directional_pos
+impl Action {
+    fn from_line(line: &str) -> Vec<Action> {
+        let mut ret = Vec::new();
+        let mut i = 3;
+        let mut j = 2;
+        for &c in line.as_bytes() {
+            let (ti, tj) = NUM_KEYPAD
                 .iter()
-                .map(|pos| pos.dist(DIR_START))
-                .sum::<i32>()
-    }
-}
-
-impl<const N: usize> PartialOrd for CostState<N> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<const N: usize> Ord for CostState<N> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.f().cmp(&self.f())
-    }
-}
-
-pub fn type_on_keypad<const N: usize>(line: &str) -> usize {
-    let line = line.as_bytes();
-    let mut q: BinaryHeap<CostState<N>> = BinaryHeap::new();
-    q.push(CostState::new());
-    let mut visited = HashSet::new();
-    while let Some(CostState { cost, state }) = q.pop() {
-        if !visited.insert(state.clone()) {
-            continue;
+                .enumerate()
+                .find_map(|(di, row)| {
+                    row.iter()
+                        .position(|&x| x == c)
+                        .map(|dj| (di as i8, dj as i8))
+                })
+                .unwrap();
+            ret.push(Action {
+                di: ti - i,
+                dj: tj - j,
+                a: 1,
+            });
+            i = ti;
+            j = tj;
         }
-        for &action in b"<>^vA" {
-            let new_cost = cost + 1;
-            if let Some(new_state) = state.act(line, action) {
-                if new_state.typed == line.len() as i32 {
-                    return new_cost as usize;
-                }
-                q.push(CostState {
-                    cost: new_cost,
-                    state: new_state,
+        ret
+    }
+
+    /// Return the higher level actions needed to perform the given action.
+    fn perform(&self) -> Vec<Action> {
+        let &Action { di, dj, a } = self;
+        let mut ret = Vec::new();
+        //     +---+---+
+        //     | ^ | A |
+        // +---+---+---+
+        // | < | v | > |
+        // +---+---+---+
+        if di < 0 {
+            // Move from A to ^: <, followed by -di presses of A
+            ret.push(Action {
+                di: 0,
+                dj: -1,
+                a: -di,
+            });
+            if dj < 0 {
+                // Move from ^ to <: v<, followed by -dj presses of A
+                ret.push(Action {
+                    di: 1,
+                    dj: -1,
+                    a: -dj,
                 });
+                // Move from < to A: >>^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 2, a });
+            } else if dj == 0 {
+                // Move from ^ to A: >, followed by a presses of A
+                ret.push(Action { di: 0, dj: 1, a });
+            } else {
+                // Move from ^ to >: v>, followed by dj presses of A
+                ret.push(Action {
+                    di: 1,
+                    dj: 1,
+                    a: dj,
+                });
+                // Move from > to A: ^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 0, a });
+            }
+        } else if di == 0 {
+            if dj < 0 {
+                // Move from A to <: v<<, followed by -dj presses of A
+                ret.push(Action {
+                    di: 1,
+                    dj: -2,
+                    a: -dj,
+                });
+                // Move from < to A: >>^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 2, a });
+            } else if dj == 0 {
+                // Don't move, followed by a presses of A
+                ret.push(Action { di: 0, dj: 0, a });
+            } else {
+                // Move from A to >: v, followed by dj presses of A
+                ret.push(Action {
+                    di: 1,
+                    dj: 0,
+                    a: dj,
+                });
+                // Move from > to A: ^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 0, a });
+            }
+        } else {
+            // Move from A to v: v<, followed by -di presses of A
+            ret.push(Action {
+                di: 1,
+                dj: -1,
+                a: di,
+            });
+            if dj < 0 {
+                // Move from v to <: <, followed by -dj presses of A
+                ret.push(Action {
+                    di: 0,
+                    dj: -1,
+                    a: -dj,
+                });
+                // Move from < to A: >>^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 2, a });
+            } else if dj == 0 {
+                // Move from v to A: >^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 1, a });
+            } else {
+                // Move from v to >: >, followed by dj presses of A
+                ret.push(Action {
+                    di: 0,
+                    dj: 1,
+                    a: dj,
+                });
+                // Move from > to A: ^, followed by a presses of A
+                ret.push(Action { di: -1, dj: 0, a });
             }
         }
+        ret
     }
-    panic!("No solution found");
+
+    fn presses(&self) -> usize {
+        (self.di.abs() + self.dj.abs() + self.a.abs()) as usize
+    }
+}
+
+// Fails on last line of example:
+
+// [^A,                 ^^<<A,                                     >>A,               vvvA]
+// [<A,         >A,     <AA,         v<AA,           ^>>A,         vAA,       ^A,     v<AAA,           ^>A]
+// [v<<A, ^>>A, vA, ^A, v<<A, ^>>AA, v<A, <A, ^>>AA, <A, v>AA, ^A, v<A, ^>AA, <A, >A, v<A, <A, ^>>AAA, <A, v>A, ^A]
+//
+// [^A,                 <<^^A,                                 >>A,               vvvA
+// [<A,         >A,     v<<AA,           >^AA,         >A,     vAA,       ^A,     <vAAA,           >^A]
+// [<v<A, >>^A, vA, ^A, <vA, <AA, >>^AA, vA, <^A, >AA, vA, ^A, <vA, >^AA, <A, >A, <v<A, >A, >^AAA, vA, <^A, >A]
+
+// [^^<<A,
+// [<AA,         v<AA,           ^>>A
+// [v<<A, ^>>AA, v<A, <A, ^>>AA, <A, v>AA, ^A
+//  9            10              8
+//
+// [<<^^A,
+// [v<<AA,           >^AA,         >A
+// [<vA, <AA, >>^AA, vA, <^A, >AA, vA, ^A
+//  11               8             4
+
+impl std::fmt::Debug for Action {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let &Action { di, dj, a } = self;
+        for _ in 0..di.abs() {
+            if di > 0 {
+                write!(f, "v")?;
+            } else {
+                write!(f, "^")?;
+            }
+        }
+        for _ in 0..dj.abs() {
+            if dj > 0 {
+                write!(f, ">")?;
+            } else {
+                write!(f, "<")?;
+            }
+        }
+        for _ in 0..a.abs() {
+            write!(f, "A")?;
+        }
+        Ok(())
+    }
+}
+
+// [^A, ^^<<A, >>A, vvvA]
+// [<A, >A, <AA, v<AA, ^>>A, vAA, ^A, v<AAA, ^>A]
+// [v<<A, ^>>A, vA, ^A, v<<A, ^>>AA, v<A, <A, ^>>AA, <A, v>AA, ^A, v<A, ^>AA, <A, >A, v<A, <A, ^>>AAA, <A, v>A, ^A]
+
+pub fn type_on_keypad(line: &str, n_robots: usize) -> usize {
+    let mut actions = Action::from_line(line);
+    println!("{:?}", actions);
+    for _ in 0..n_robots {
+        let mut new_actions = Vec::new();
+        for action in &actions {
+            new_actions.extend(action.perform());
+        }
+        actions = new_actions;
+        println!("{:?}", actions);
+    }
+    actions.into_iter().map(|action| action.presses()).sum()
 }
 
 pub fn part1(input: &str) -> impl Display {
     let mut total = 0;
     for line in input.lines() {
-        let length = type_on_keypad::<2>(line);
+        let length = type_on_keypad(line, 2);
         let numeric = line.strip_suffix("A").unwrap();
         let value: usize = numeric.parse().unwrap();
+        println!("{length} * {value} = {}", length * value);
         total += length * value;
     }
     total
@@ -228,7 +281,7 @@ pub fn part1(input: &str) -> impl Display {
 pub fn part2(input: &str) -> impl Display {
     let mut total = 0;
     for line in input.lines() {
-        let length = type_on_keypad::<25>(line);
+        let length = type_on_keypad(line, 25);
         let numeric = line.strip_suffix("A").unwrap();
         let value: usize = numeric.parse().unwrap();
         total += length * value;
